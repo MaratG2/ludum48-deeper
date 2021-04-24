53928725b4f6e1bf84e86a0841152dbbbbbfbfcf
Marat <maratg2develop@gmail.com>
2021-04-24 13:27:55 +0400

    Base controller

---
+++ Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    void Awake()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 60;
+    }
+
+    void Update()
+    {
+        if (Application.targetFrameRate != 60)
+            Application.targetFrameRate = 60;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Boat : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Range(0.5f, 10f)] [SerializeField] private float horizontalSpeed = 1f;
+    [Range(1f, 10f)] [SerializeField] private float verticalSpeed = 1f;
+
+    private Rigidbody2D rb2d;
+    private SpriteRenderer sprite;
+
+    private float horizontalInput;
+    private float horizontalInputRaw;
+    private float verticalInput;
+    private float verticalInputPos;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        sprite = GetComponentInChildren<SpriteRenderer>();
+    }
     void Start()
     {
         
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        horizontalInputRaw = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        if (verticalInput >= 0)
+            verticalInputPos = verticalInput * verticalSpeed;
+        else
+            verticalInputPos = 0;
+
+        if (horizontalInputRaw >= 0)
+            sprite.flipX = true;
+        else
+            sprite.flipX = false;
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.velocity = new Vector2(horizontalInput, -0.5f + verticalInputPos);
+
     }
 }
