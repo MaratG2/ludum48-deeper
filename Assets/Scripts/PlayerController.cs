d68c5b84f6826f62c3684b32458d5d63cdda016e
Marat <maratg2develop@gmail.com>
2021-04-26 18:40:55 +0400

    Submarine flip

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,7 +23,7 @@ public class PlayerController : MonoBehaviour
     float angle = 0f;
     float angleBase;
     float wasAdd = 0;
-    float addAngle = 0f;
+    float addAngle = 180f;
 
     void Awake()
     {
@@ -57,52 +57,29 @@ public class PlayerController : MonoBehaviour
     {
         rb2d.angularVelocity = 0;
 
-        //mouse_pos = Input.mousePosition;
-        //mouse_pos.z = 5.23f; //The distance between the camera and object
-        //object_pos = Camera.main.WorldToScreenPoint(transform.position);
-        //mouse_pos.x = mouse_pos.x - object_pos.x;
-        //mouse_pos.y = mouse_pos.y - object_pos.y;
-        //angle = Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg;
-
-        //Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Vector2 direction3 = (mouseScreenPosition - (Vector2)transform.position).normalized;  
-        //transform.right = Vector2.Lerp(transform.right, -direction3, Time.deltaTime * smoothCam);
-        //Debug.Log(transform.right.x == -1f && transform.right.y == 0f);
-
         var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
-        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg + 180f;
-        Quaternion newRot = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * smoothCam);
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (angle >= -90f && angle <= 90f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); //maybe need to delete
+            addAngle = 0f;
+        }
+        else
+        {
+            transform.localScale = new Vector3(1, 1, 1); //maybe need to delete
+            addAngle = -180f;
+        }
+        angleBase = angle + addAngle;
+        Quaternion newRot = Quaternion.AngleAxis(angleBase, Vector3.forward);
+        
+        if (wasAdd == addAngle)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, newRot, Time.deltaTime * smoothCam);
+        }
+        else
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angleBase));
 
-        //if (angle >= -90f && angle <= 90f)
-        //{
-        //    transform.localScale = new Vector3(-1, 1, 1); //maybe need to delete
-        //    addAngle = 0f;
-        //}
-        //else
-        //{
-        //    transform.localScale = new Vector3(1, 1, 1); //maybe need to delete
-        //    addAngle = -180f;
-        //}
-        //angleBase = angle + addAngle;
-
-        //if (wasAdd == addAngle)
-        //{
-
-        //    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(new Vector3(0, 0, angleBase)), Time.deltaTime * smoothCam);
-
-        //    //Debug.Log(Quaternion.ToEulerAngles(transform.rotation).z);
-        //}
-        //else
-        //    transform.rotation = Quaternion.Euler(new Vector3(0, 0, angleBase));
-
-        //var direction = Quaternion.Euler(new Vector3(0, 0, angleBase)) * transform.right;
-        //var direction2 = transform.rotation * transform.right;
-        //Debug.DrawRay(transform.position, direction * 150f, Color.red, 1f);
-        //Debug.DrawRay(transform.position, direction2 * 150f, Color.yellow, 1f);
-
-        //wasAdd = addAngle;
-
+        wasAdd = addAngle;
         return angle;
     }
 }
